2f5a1b652f3719bc222aba25c9f237209060f7f8
FabienBounoir <bounoirfabien@gmail.com>
2024-10-04 22:31:23 +0200

    feat: 🎸 Add privacy and terms when use pixelouns

---
+++ src/routes/privacy/+page.svelte
@@ -0,0 +1,257 @@
+ <div class="privacy">
+            <div class="privacy-section">
+                <h1><b>Pixelouns</b> Privacy Policy</h1>
+            </div>
+
+
+            <div class="privacy-section">
+                <p class="last-update">
+                    <strong>Last update</strong>: 4 octobre 2024
+                </p>
+                <p>
+                    This privacy policy applies to the Discord bot <b>Pixelouns#7406</b>.
+                </p>
+
+                <p><b>Pixelouns</b> is a French Activities that aims to <b>improve your experience</b> in your discord, to complete this project, we need to collect some personal data.</p>
+                <h2>Why we collect data</h2>
+                <p>
+                    We collect data for the sole purpose of providing you with a <b>quality service</b>.
+                </p>
+
+                <h2>The data we collect</h2>
+                <p>
+                    <b>Pixelouns</b> collects only the data essential to its proper functioning, here are the details of these data:
+                </p>
+                <section>
+                    <h5>The data on your discord server</h5>
+                    <p>
+                        <b>Pixelouns</b> needs to collect some data on the discord server to be able to work, these data are :
+                    </p>
+                    <ul>
+                        <li>The server ID</li>
+                        <li>The configuration of <b>Pixelouns</b> within the server:<br />
+                            - Pixelwar color<br />
+                            - Pixelwar size<br />
+                            - Cooldown<br />
+                            - End date<br />
+                        </li>
+                    </ul>
+                </section>
+                <section>
+                    <h5>Your discord account data</h5>
+                    <p>
+                        <b>Pixelouns</b> needs to collect some data on the discord account so that it knows who you are, to be able to provide you with an optimized and quality service according to the user, these data are the following:
+                    </p>
+                    <ul>
+                        <li>Your discord account ID</li>
+                        <li>The global name of your discord account</li>
+                        <li>The link of your discord avatar</li>
+                        <li>Information on whether or not the user agrees to the collection of their data</li>
+                    </ul>
+                </section>
+
+                <h2>How the data is used</h2>
+                <p>
+                    The data is used to provide you with a quality service, here are the different cases of data use:
+                </p>
+                <section>
+                    <h5>The data on your discord server</h5>
+                    <p>
+                        The data of your discord server are used to provide you a quality service. We use the data collected for the sole purpose of having the configuration of <b>Pixelouns</b> within your discord server, but also the xp and the number of messages of the members to be able to provide you roles according to the levels that the members can have but also to display the rankings of the members within your discord server.
+                    </p>
+                </section>
+                <section>
+                    <h5>Your discord account data</h5>
+                    <p>
+                        The data from your discord account is used for the sole purpose of providing you with a quality service, we use the data collected to display your profile on the pixel you are on.
+                    </p>
+                </section>
+
+                <h2>How data is shared</h2>
+                <p>
+                    <b>NO data stored</b> is shared with third parties, they are exclusive to the use of <b>Pixelouns</b>. The data are stored on a private server, and are accessible only by the developers of <b>Pixelouns</b>.
+                </p>
+
+                <h2>How to contact us</h2>
+                <p>
+                    The <b>Pixelouns</b> team wants to be transparent with the use of your data, that's why we put in place many ways for you to contact us, here are the different ways to contact us:
+                </p>
+                <section>
+                    <h5>Discord</h5>
+                    <p>
+                        You can contact us on our discord server, for that you just have to join the server of <b>Pixelouns</b> by clicking on the button below:
+                    </p>
+                    <a href="https://discord.gg/8QZ7Y4K" target="_blank" rel="noopener noreferrer">
+                        <button class="btn btn-primary linkButton">Join the discord server</button>
+                    </a>
+                </section>
+                <section>
+                    <h5>Mail</h5>
+                    <p>
+                        You can contact us by mail, for that you just have to send it to the following address
+                    </p>
+                    <a href="mailto:bounoirfabien@gmail.com">
+                        <button class="btn btn-primary linkButton">Send an email</button>
+                    </a>
+                </section>
+
+                <h2>How to delete your data</h2>
+                <p>
+                    <b>Pixelouns</b> was created with the sole purpose of improving your experience on discord, and we do not want your data to be misused, that's why we put in place several ways to view your data collected, as well as to delete them if you wish.
+                </p>
+                <section>
+                    <h5>Delete your data</h5>
+                    <p>
+                        You can also delete your data by contacting us by mail or on our discord server, we will delete them as soon as possible.
+                    </p>
+                </section>
+                <section>
+                    <h5>Visualize your data</h5>
+                    <p>
+                        You can view your data by doing send email to the following address: 
+                        <a href="mailto:bounoirfabien@gmail.com">
+                        <button class="btn btn-primary linkButton">Send an email</button>
+                    </a>
+                    </p>
+                </section>
+                <p>
+                    Also see <a href="https://discord.com/privacy">Discord’s Privacy Policy.</a>
+                </p>
+
+                <div class="signature">
+                    <p>
+                        <b>Thanks for using Pixelouns!</b>
+                    </p>
+                </div>
+            </div>
+        </div>
+
+<style>
+    .privacy {
+    padding: 0 20px;
+    margin: 0 auto;
+    font-size: 16px;
+    line-height: 1.5;
+    color: white;
+    background: var(--back-first-color);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+
+.privacy-section {
+    max-width: 800px;
+    padding: 2rem;
+    font-size: 1.1rem;
+}
+
+
+.privacy-section {
+    text-align: left;
+}
+
+.privacy-section:first-child {
+    text-align: center;
+}
+
+.privacy-section ul {
+    display: block;
+}
+
+.privacy-section h1, h2, h3, h4, h5, h6 {
+    color: var(--color-principal);
+    font-weight: 600 !important;
+}
+
+.privacy-section h1 {
+    font-family: Satoshi, sans-serif;
+    color: #fff;
+}
+
+.privacy-section h5 {
+    display: list-item;
+    list-style-type: disc;
+}
+
+.privacy-section section {
+    display: block;
+    margin-left: 2em;
+    background: transparent;
+}
+
+.privacy-section .linkButton {
+    display: block;
+    margin-top: 0rem;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: var(--color-principal);
+    border: 1px solid var(--color-principal-hover);
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+    font-size: 1.1rem;
+    font-weight: 600;
+    transition-property: background-color, border;
+    transition-duration: 0.3s;
+    transition-timing-function: ease;
+}
+
+.privacy-section .signature {
+    display: block;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: var(--color-principal);
+    border: 1px solid var(--color-principal-hover);
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+    font-size: 1.1rem;
+    font-weight: 600;
+    text-align: center;
+}
+
+.privacy-section .signature p {
+    margin: 1em;
+}
+
+.privacy .last-update {
+    padding: 0;
+    margin: 0 0 1.3em 0;
+    font-style: italic;
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+}
+
+.privacy-section .signature b {
+    color: white;
+}
+
+.privacy-section a {
+    text-decoration: none;
+}
+
+.privacy-section code {
+    background-color: var(--back-second-color);
+    color: var(--color-principal);
+    padding: 0.2rem 0.3rem;
+    border-radius: 5px;
+    margin: 0 0.1em;
+    white-space: nowrap;
+}
+
+.privacy-section .linkButton:hover {
+    background-color: var(--color-principal-hover);
+    border: 1px solid var(--color-principal);
+}
+
+
+@media screen and (max-width: 768px) {
+    .privacy-section {
+        padding: 1rem;
+    }
+}
+</style>
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,218 @@
+<div class="terms">
+    <div class="terms-section">
+        <h1><b>Pixelouns</b> Terms of Service</h1>
+    </div>
+    <div class="terms-section">
+        <p class="last-update">
+            <strong>Effective</strong> october 4, 2024
+        </p>
+        <p>
+            This Terms of Service applies to the Discord bot <b>Pixelouns#7406</b>.
+        </p>
+        <p>
+            Welcome to Pixelouns. Please read on to learn the rules and restrictions that govern your use of our proprietary bots. If you have any questions, comments, or concerns regarding these terms or the Services, please contact us at our Discord support server.
+        </p>
+        <p>
+            These Terms of Use (the "Terms”) are a binding contract between you and Pixelouns. ("Pixelouns" "we" and "us”).
+            Your use of the Services in any way means that you agree to all of these Terms, and these Terms will remain in effect while you use the Services. These Terms include the provisions in this document as well as those in the Privacy Policy and Copyright Dispute Policy.
+        </p>
+        <p>
+            You may not use Pixelouns to violate any applicable laws or regulations, Discord’s Terms of Service, or Discord Community Guidelines. You also may not use Pixelouns to harm anyone or anything.
+        </p>
+
+        <h2>What about my privacy?</h2>
+        <p>
+            We collect data for the sole purpose of providing you with a <b>quality service</b>.
+        </p>
+        <p>
+            Pixelouns takes the privacy of its users very seriously. For the current Privacy Policy, please <a href="/bot/privacy">click here</a>.
+        </p>
+
+        <section>
+            <h5>
+                Children’s Online Privacy Protection Act (COPPA)
+            </h5>
+
+            <p>
+                Pixelouns does not knowingly collect any personal information from children under the age of 13. If you are under the age of 13, please do not submit any personal information through Pixelouns. We encourage parents and legal guardians to monitor their children’s Internet usage and to help enforce this Policy by instructing their children never to provide personal information through Pixelouns without their permission. If you have reason to believe that a child under the age of 13 has provided personal information to Pixelouns through its Services, please contact us, and we will endeavor to delete that information from our databases.
+            </p>
+        </section>
+
+        <h2>
+            What are the basics of using Pixelouns?
+        </h2>
+        <p>
+            You may need to access certain parts or features of the Services (e.g. our proprietary bots) by using your account credentials and password from other services ("Third Party Account”), such as those offered by Discord. By using the Services through a Third Party Account, you permit us to access certain information from such account for use by the Services. You are ultimately in control of how much information is accessible to us and may exercise such control by adjusting your privacy settings on your Third Party Account. We store your account credentials but will not store your password. You promise to provide us with accurate, complete, and updated registration information about yourself. You may not use any Third Party Account that you do not have the right to use, or another person’s account credentials without authorization from that other person. You may not transfer your account to anyone else without our prior written permission.
+        </p>
+        <p>
+            You represent and warrant that you are an individual of legal age to form a binding contract (or if not, you’ve received your parent’s or guardian’s permission to use the Services and have gotten your parent or guardian to agree to these Terms on your behalf). If you’re agreeing to these Terms on behalf of an organization or entity, you represent and warrant that you are authorized to agree to these Terms on that organization’s or entity’s behalf and bind them to these Terms (in which case, the references to "you” and "your” in these Terms, except for in this sentence, refer to that organization or entity).
+        </p>
+        <section>
+            <h5>
+                You may not use the Services to do any of the following:
+            </h5>
+            <ul>
+                <li>
+                    Use the Services in any way that violates any applicable federal, state, local, or international law or regulation.
+                </li>
+                <li>
+                    Use the Services in any way that is unlawful, harmful, threatening, abusive, harassing, tortious, defamatory, vulgar, obscene, libelous, invasive of another’s privacy, hateful, or racially, ethnically, or otherwise objectionable.
+                </li>
+                <li>
+                    Use the Services to impersonate any person or entity, including any of our employees or representatives.
+                </li>
+                <li>
+                    Use the Services to engage in any other conduct that restricts or inhibits anyone’s use or enjoyment of the Services, or which, as determined by us, may harm or offend us or users of the Services or expose them to liability.
+                </li>
+            </ul>
+        </section>
+
+        <h2>
+            Changes to the Terms
+        </h2>
+
+        <p>
+            We may revise these Terms from time to time. The most current version of the Terms will supersede all previous versions. Pixelouns encourages you to periodically review these Terms to stay informed of our updates.
+        </p>
+
+        <div class="signature">
+            <p>
+                <b>Thanks for using Pixelouns!</b>
+            </p>
+        </div>
+
+
+
+    </div>
+
+
+</div>
+
+<style>
+    .terms {
+    padding: 0 20px;
+    margin: 0 auto;
+    font-size: 16px;
+    line-height: 1.5;
+    color: white;
+    background: var(--back-first-color);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+}
+
+
+.terms-section {
+    max-width: 800px;
+    padding: 3rem;
+    font-size: 1.1rem;
+}
+
+.terms-section {
+    text-align: left;
+}
+
+.terms-section:first-child {
+    text-align: center;
+}
+
+.terms-section ul {
+    display: block;
+}
+
+.terms-section h1, h2, h3, h4, h5, h6 {
+    color: var(--color-principal);
+    font-weight: 600 !important;
+}
+
+.terms-section h5 {
+    display: list-item;
+    list-style-type: disc;
+}
+
+.terms-section section {
+    display: block;
+    margin-left: 2em;
+    background: transparent;
+}
+
+.terms-section .linkButton {
+    display: block;
+    margin-top: 0rem;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: var(--color-principal);
+    border: 1px solid var(--color-principal-hover);
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+    font-size: 1.1rem;
+    font-weight: 600;
+
+    transition-property: background-color, border-color;
+    transition-duration: 0.3s;
+    transition-timing-function: ease;
+}
+
+.terms-section .signature {
+    display: block;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    padding: 0.5rem 1rem;
+    background-color: var(--color-principal);
+    border: 1px solid var(--color-principal-hover);
+    border-radius: 5px;
+    color: white;
+    text-decoration: none;
+    font-size: 1.1rem;
+    font-weight: 600;
+    text-align: center;
+}
+
+.terms-section .signature p {
+    margin: 1em;
+}
+
+
+.terms-section .signature b {
+    color: white;
+}
+
+.terms-section .signature button {
+    background-color: white;
+    border: 1px solid var(--color-principal-hover);
+    color: var(--color-principal);
+}
+
+
+
+.terms-section a {
+    text-decoration: none;
+}
+
+.terms-section code {
+    background-color: var(--back-second-color);
+    color: var(--color-principal);
+    padding: 0.2rem 0.5rem;
+    border-radius: 5px;
+    margin: 0 0.1em;
+}
+
+.terms-section .linkButton:hover {
+    background-color: var(--color-principal-hover);
+    border: 1px solid var(--color-principal);
+}
+
+.terms-section h1 {
+    font-family: Satoshi, sans-serif;
+    color: #fff;
+}
+
+
+@media screen and (max-width: 768px) {
+    .terms-section {
+        padding: 1rem;
+    }
+}
+</style>
